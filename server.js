7a016b70bab2959473fcefc40d27314ac1933e22
KnightCorporation <92050554+KnightCorporation@users.noreply.github.com>
2022-01-05 14:53:16 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, 'build')));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
